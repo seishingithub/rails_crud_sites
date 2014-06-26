51860fd2df783636b74aa1d0007af7136ceb6643
Peggy Griffin <seishingithub@gmail.com>
2014-06-25 21:36:53 -0600

    User can edit and update list of tourist sites

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -14,6 +14,21 @@ class SitesController < ApplicationController
     end
   end
 
+  def show
+    @site = Site.find(params[:id])
+  end
+
+  def edit
+    @site = Site.find(params[:id])
+  end
+
+  def update
+    @site = Site.find(params[:id])
+    @site.update_attributes!(site_params)
+
+    redirect_to sites_path
+  end
+
   private
   def site_params
     params.require(:site).permit(:site_name, :location)
--- spec/features/sites_spec.rb
+++ spec/features/sites_spec.rb
@@ -11,4 +11,23 @@ feature 'Manage Sites' do
     expect(page).to have_content 'Eiffel Tower'
     expect(page).to have_content 'Paris, France'
   end
+
+  scenario 'User can edit and update list of tourist sites' do
+    visit '/'
+    click_on 'Add Tourist Site'
+    fill_in 'Site name', with: 'Eiffel Tower'
+    fill_in 'Location', with: 'Paris, France'
+    click_on 'Create Tourist Site'
+    expect(page).to have_content 'Eiffel Tower'
+    expect(page).to have_content 'Paris, France'
+    click_on 'Eiffel Tower'
+    expect(page).to have_content 'Eiffel Tower'
+    expect(page).to have_content 'Paris, France'
+    click_on 'Edit'
+    fill_in 'Site name', with: 'Golden Gate Bridge'
+    fill_in 'Location', with: 'San Fran, CA'
+    click_on 'Update'
+    expect(page).to have_content 'Golden Gate Bridge'
+    expect(page).to have_content 'San Fran, CA'
+  end
 end
